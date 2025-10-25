d4b76110ef66dcc1bd5326d3d5eb85e99e9e598f
manIlovefr0gs <151011077+manIlovefr0gs@users.noreply.github.com>
2025-10-25 17:19:55 +0200

    Removed comments and print statements

--- introduction/set_joint_commands.py
+++ introduction/set_joint_commands.py
@@ -20,12 +20,9 @@ from spark_agent import SparkAgent
 class MyAgent(SparkAgent):
     def think(self, perception):
         action = super(MyAgent, self).think(perception)
-        # YOUR CODE HERE
+        
         action.stiffness['LShoulderPitch'] = 0
         action.speed['HeadYaw'] = 0.1
-
-        print(action.stiffness)
-        print(action.speed)
 
         return action
 
