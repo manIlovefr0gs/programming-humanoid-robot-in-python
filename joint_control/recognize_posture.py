c44685eb24cb39e9cdbaed88faeb4b16573e0d8c
kermit <dreher.tu@gmail.com>
2025-11-13 21:28:50 +0100

    implemented recognize_posture -> tested successfully for posture standing

--- joint_control/recognize_posture.py
+++ joint_control/recognize_posture.py
@@ -11,7 +11,10 @@
 
 
 from angle_interpolation import AngleInterpolationAgent
-from keyframes import hello
+from keyframes import *
+import numpy as np
+import pickle
+from sklearn import svm
 
 
 class PostureRecognitionAgent(AngleInterpolationAgent):
@@ -22,17 +25,48 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
                  sync_mode=True):
         super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.posture = 'unknown'
-        self.posture_classifier = None  # LOAD YOUR CLASSIFIER
+        with open('robot_pose.pkl', 'rb') as f:
+            self.posture_classifier = pickle.load(f)
+        
+        if isinstance(self.posture_classifier, svm.SVC):
+            print("Classifier loaded successfully.")
+        else:
+            print("Error loading the classifier!")
+
 
     def think(self, perception):
+        #print("Attributes of perception object:", dir(perception))
+        #print("Current values in perception object:", perception.__dict__)
+        print("Posture:", self.posture)
         self.posture = self.recognize_posture(perception)
         return super(PostureRecognitionAgent, self).think(perception)
 
+
+
     def recognize_posture(self, perception):
         posture = 'unknown'
-        # YOUR CODE HERE
 
+        features = perception_to_features(perception).reshape(1, -1)  
+        predicted_class = self.posture_classifier.predict(features)[0]
+        return str(predicted_class)
         return posture
+
+
+def perception_to_features(perception):
+
+    features = [
+        perception.joint['LHipYawPitch'],
+        perception.joint['LHipRoll'],
+        perception.joint['LHipPitch'],
+        perception.joint['LKneePitch'],
+        perception.joint['RHipYawPitch'],
+        perception.joint['RHipRoll'],
+        perception.joint['RHipPitch'],
+        perception.joint['RKneePitch'],
+        perception.imu[0],  
+        perception.imu[1]   
+        ]
+    return np.array(features)
 
 if __name__ == '__main__':
     agent = PostureRecognitionAgent()
